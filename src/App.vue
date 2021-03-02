3633270b6f38c3a788504f30252dcb59409f5761
Alex <alejandro.munoz@student.uts.edu.au>
2021-03-02 12:53:59 +1100

    setup base app

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="header">Todo App</h1>
+    <todo-list />
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  background-image: linear-gradient(to bottom right, rgb(151, 206, 79), rgb(39, 255, 208));
+  height: 100%;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+  
+}
+
+.header {
+  font-size: 30px;
+  color: rgb(255, 255, 255);
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  
+  
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <input type="text" class="todo-input" placeholder="Add item here" v-model="newTodo" @keyup.enter="addTodo"/>
+        <div v-for="todo in todos" :key="todo.id" class="todo-item">
+            {{todo.title}}
+            <button>x</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'todo-list',
+    data() {
+        return {
+            newTodo: '',
+            newId: 2,
+            todos: [
+                {
+                    'id': 1,
+                    'title': 'First todo item',
+                    'completed': false
+                }
+            ]
+        }
+    },
+    methods: {
+        addTodo(){
+            if (this.newTodo.trim().length == 0)
+            {
+                return
+            }
+
+            this.todos.push({
+                id: this.newId,
+                title: this.newTodo,
+                completed: false
+            })
+
+            this.newTodo = '';
+            this.newId++;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;   
+}
+
+.todo-item {
+    display: flex;
+    justify-content: space-between;
+    border: solid 1px;
+    padding: 10px;
+}
+</style>
